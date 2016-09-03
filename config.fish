504af525c07716e6cf38a599e0d4248026ef94f7
pavelsof <pavelsof@gmail.com>
2016-09-03 22:27:12 +0300

    Fish.

---
+++ config.fish
@@ -0,0 +1,15 @@
+
+# disable error highlighting
+set fish_color_error $fish_color_normal
+
+# make HOME and END work in fish
+function fish_user_key_bindings
+	bind \e\[1~ beginning-of-line
+	bind \e\[4~ end-of-line
+end
+
+# fix gvim on ubuntu bug: see ~/.bashrc
+function gvim
+	command gvim ^ /dev/null $argv
+end
+
