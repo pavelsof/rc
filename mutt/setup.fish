0e9b288477767ef0762f18eb5c70a8217fb3f3ca
pavelsof <pavelsof@gmail.com>
2018-05-27 22:42:43 +0200

    mutt: smoother multi-accounting.

--- mutt/setup.fish
+++ mutt/setup.fish
@@ -1,11 +1,9 @@
 #!/usr/bin/fish
 
-begin
-	set -l mutt_dir (realpath (dirname (status -f)))
+set -l mutt_dir (realpath (dirname (status -f)))
 
-	if not test -d $HOME/.mutt
-		mkdir -v $HOME/.mutt
-	end
-
-	ln -sv $mutt_dir/muttrc $HOME/.mutt/muttrc
+if not test -d $HOME/.mutt
+	mkdir -v $HOME/.mutt
 end
+
+ln -sv $mutt_dir/muttrc $HOME/.mutt/muttrc
