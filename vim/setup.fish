a39dd0e67e901d87b5f3638cdf2298a635d1b775
pavelsof <pavelsof@gmail.com>
2018-05-28 15:18:53 +0200

    vim: stab at wild ignorance.

--- vim/setup.fish
+++ vim/setup.fish
@@ -1,9 +1,7 @@
 #!/usr/bin/fish
 
-begin
-	set -l vim_dir (realpath (dirname (status -f)))
+set -l vim_dir (realpath (dirname (status -f)))
 
-	ln -sv $vim_dir/vim $HOME/.vim
-	ln -sv $vim_dir/vimrc $HOME/.vimrc
-	ln -sv $vim_dir/gvimrc $HOME/.gvimrc
-end
+ln -sv $vim_dir/vim $HOME/.vim
+ln -sv $vim_dir/vimrc $HOME/.vimrc
+ln -sv $vim_dir/gvimrc $HOME/.gvimrc
