acefbc503057803186bf3f9eec89f2845ed615ef
pavelsof <pavelsof@gmail.com>
2018-06-15 18:32:28 +0200

    vim: more aggressive setup.fish.

--- vim/setup.fish
+++ vim/setup.fish
@@ -2,6 +2,9 @@
 
 set -l vim_dir (realpath (dirname (status -f)))
 
+# ~/.vim is likely to be there already
+rm -r $HOME/.vim
+
 ln -sv $vim_dir/vim $HOME/.vim
 ln -sv $vim_dir/vimrc $HOME/.vimrc
 ln -sv $vim_dir/gvimrc $HOME/.gvimrc
