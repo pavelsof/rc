154d1053e16a918a21abc99791afa4fa9ee6122f
pavelsof <pavelsof@gmail.com>
2019-05-08 14:16:41 +0200

    i3: accumulated updates.

--- i3/setup.fish
+++ i3/setup.fish
@@ -1,13 +1,11 @@
-#!/usr/bin/fish
+#!/usr/bin/env fish
 
-begin
-	set -l i3_dir (realpath (dirname (status -f)))
+set -l i3_dir (realpath (dirname (status -f)))
 
-	if not test -d $HOME/.config/i3
-		mkdir -pv $HOME/.config/i3
-	end
-
-	ln -sv $i3_dir/config $HOME/.config/i3/config
-
-	touch $HOME/.config/i3/post-config.sh
+if not test -d $HOME/.config/i3
+	mkdir -pv $HOME/.config/i3
 end
+
+ln -sv $i3_dir/config $HOME/.config/i3/config
+
+touch $HOME/.config/i3/post-config.sh
