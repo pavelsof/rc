022f339e102322040f5a8f57e2821b7fbbff23dc
pavelsof <pavelsof@gmail.com>
2019-07-16 11:06:54 +0200

    mutt: ~/.mutt → ~/.config/mutt.

--- mutt/setup.fish
+++ mutt/setup.fish
@@ -2,8 +2,9 @@
 
 set -l mutt_dir (realpath (dirname (status -f)))
 
-if not test -d $HOME/.mutt
-	mkdir -v $HOME/.mutt
+if not test -d $HOME/.config/mutt
+	mkdir -pv $HOME/.config/mutt
 end
 
-ln -sv $mutt_dir/muttrc $HOME/.mutt/muttrc
+ln -sv $mutt_dir/muttrc $HOME/.config/mutt/muttrc
+touch $HOME/.config/mutt/muttrc_local
