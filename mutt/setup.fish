0df74abe86d1a08c040f760c6e1cca33f77e149e
pavelsof <pavelsof@gmail.com>
2019-11-10 21:38:45 +0100

    mutt: more Gmail-like.

--- mutt/setup.fish
+++ mutt/setup.fish
@@ -1,10 +1,25 @@
 #!/usr/bin/env fish
 
-set -l mutt_dir (realpath (dirname (status -f)))
+set -l repo_dir (realpath (dirname (status -f)))
+set -l config_dir $HOME/.config/mutt
 
-if not test -d $HOME/.config/mutt
-	mkdir -pv $HOME/.config/mutt
+if test -d $config_dir
+	echo $config_dir → OK
+else
+	mkdir -p $config_dir
+	echo $config_dir → created
 end
 
-ln -sv $mutt_dir/muttrc $HOME/.config/mutt/muttrc
-touch $HOME/.config/mutt/muttrc_local
+if test -e $config_dir/muttrc
+	echo $config_dir/muttrc → OK
+else
+	ln -s $repo_dir/muttrc $config_dir/muttrc
+	echo $config_dir/muttrc → symlinked
+end
+
+if test -e $config_dir/local
+	echo $config_dir/local → OK
+else
+	touch $config_dir/local
+	echo $config_dir/local → created empty
+end
