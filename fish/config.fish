71d108a0ddf4480e1c5084bd37b00c3b34eb5c6a
pavelsof <mail@pavelsof.com>
2020-12-24 15:29:15 +0200

    fish: arch-proof.

--- fish/config.fish
+++ fish/config.fish
@@ -1,37 +1,76 @@
+#
 # locale
-set -gx LANG C.UTF-8
-set -gx LC_ADDRESS C.UTF-8
-set -gx LC_COLLATE C.UTF-8
-set -gx LC_CTYPE C.UTF-8
-set -gx LC_IDENTIFICATION C.UTF-8
-set -gx LC_MONETARY C.UTF-8
-set -gx LC_MESSAGES C.UTF-8
-set -gx LC_MEASUREMENT C.UTF-8
-set -gx LC_NAME C.UTF-8
-set -gx LC_NUMERIC C.UTF-8
-set -gx LC_PAPER C.UTF-8
-set -gx LC_TELEPHONE C.UTF-8
-set -gx LC_TIME C.UTF-8
+#
+
+set -l my_locale (
+	if not test -z (locale -a | grep C.UTF-8)
+		echo C.UTF-8
+	else
+		echo en_DK.UTF-8
+	end
+)
+
+set -gx LANG $my_locale
+set -gx LC_ADDRESS $my_locale
+set -gx LC_COLLATE $my_locale
+set -gx LC_CTYPE $my_locale
+set -gx LC_IDENTIFICATION $my_locale
+set -gx LC_MONETARY $my_locale
+set -gx LC_MESSAGES $my_locale
+set -gx LC_MEASUREMENT $my_locale
+set -gx LC_NAME $my_locale
+set -gx LC_NUMERIC $my_locale
+set -gx LC_PAPER $my_locale
+set -gx LC_TELEPHONE $my_locale
+set -gx LC_TIME $my_locale
+
+
+#
+# other global vars
+#
 
 # also works for sudoedit
 set -gx EDITOR vim
 
-# disable error highlighting
-set fish_color_error $fish_color_normal
 
-# make HOME and END work in fish
-function fish_user_key_bindings
-	bind \e\[1~ beginning-of-line
-	bind \e\[4~ end-of-line
+#
+# theme fixes
+#
+
+# foreground (bold) instead of blue for commands
+set -gx fish_color_command --bold $fish_color_normal
+
+# foreground (not bold) instead of red for not yet fully typed commands
+set -gx fish_color_error $fish_color_normal
+
+
+#
+# ubuntu-specific
+#
+
+if not test -z (uname -a | grep -i ubuntu)
+
+	# make HOME and END work in fish
+	function fish_user_key_bindings
+		bind \e\[1~ beginning-of-line
+		bind \e\[4~ end-of-line
+	end
+
+	# fix the gvim issue
+	function gvim
+		command gvim ^ /dev/null $argv
+	end
+
 end
 
-# fix gvim on ubuntu bug
-function gvim
-	command gvim ^ /dev/null $argv
-end
+
+#
+# more config
+#
 
 # source config_local.fish if available
 set -l config_local (dirname (status -f))/config_local.fish
+
 if test -f $config_local
 	source $config_local
 end
