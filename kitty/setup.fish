4398c6d6877fa9cd2effb0e162e3d8efb7708e4c
pavelsof <mail@pavelsof.com>
2021-02-18 00:14:05 +0100

    kitty: allow_remote_control and the theme as a symlink.

--- kitty/setup.fish
+++ kitty/setup.fish
@@ -3,6 +3,7 @@
 set repo_dir (realpath (dirname (status -f)))
 set config_dir $HOME/.config/kitty
 
+
 if test -d $config_dir
 	echo $config_dir → already exists
 else
@@ -17,4 +18,15 @@ for item in kitty.conf themes
 		ln -s $repo_dir/$item $config_dir/$item
 		echo $config_dir/$item → symlinked
 	end
+end
+
+
+# current_theme is a symlink to the symlink of the theme
+set default_theme $config_dir/themes/gruvbox_light.conf
+
+if test -e $config_dir/current_theme
+	echo $config_dir/current_theme → already exists
+else
+	ln -s $default_theme $config_dir/current_theme
+	echo $config_dir/current_theme → created
 end
