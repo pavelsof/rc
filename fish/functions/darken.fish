be0931ec6de77369273dff75e600003ae66f5ae7
pavelsof <mail@pavelsof.com>
2021-02-27 22:13:57 +0100

    fish: second darken.

--- fish/functions/darken.fish
+++ fish/functions/darken.fish
@@ -1,10 +1,29 @@
 function darken
-	set -l config_dir ~/.config/kitty
-	set -l theme $config_dir/themes/gruvbox_dark.conf
+	set config_dir ~/.config/kitty
+	set future
+
+	switch (basename (readlink $config_dir/current_theme))
+		case gruvbox_dark.conf;
+			set future light
+		case gruvbox_light.conf
+			set future dark
+		case '*'
+			return 1
+	end
+
+	# kitty
+	set theme $config_dir/themes/gruvbox_$future.conf
 
 	for sock in (ls -1 $XDG_RUNTIME_DIR/kitty.sock*)
 		kitty @ --to unix:$sock set-colors $theme
 	end
 
 	ln -sf $theme $config_dir/current_theme
+
+	# vim
+	set -Ux THEME $future
+
+	for name in (vim --serverlist)
+		vim --servername $name --remote-send ":set background=$future<CR>"
+	end
 end
