9d9ed42307d86b7eaa91cc8506bc216520e437a0
pavelsof <mail@pavelsof.com>
2021-08-29 11:54:06 +0200

    vim: init.vim for neovim.

--- vim/setup.fish
+++ vim/setup.fish
@@ -1,10 +1,30 @@
 #!/usr/bin/env fish
 
-set -l vim_dir (realpath (dirname (status -f)))
+set this_dir (realpath (dirname (status -f)))
 
-# ~/.vim is likely to be there already
-rm -r $HOME/.vim
+# symlink ~/.vim, ~/.vimrc and ~/.gvimrc
+for item in vim vimrc gvimrc
+	set path $HOME/.$item
 
-ln -sv $vim_dir/vim $HOME/.vim
-ln -sv $vim_dir/vimrc $HOME/.vimrc
-ln -sv $vim_dir/gvimrc $HOME/.gvimrc
+	if test -e $path
+		echo $path → already exists
+	else
+		ln -s $this_dir/$item $path
+		echo $path → symlinked
+	end
+end
+
+# symlink ~/.config/nvim/init.vim
+set path $HOME/.config/nvim/init.vim
+
+if not test -e (dirname $path)
+	mkdir (dirname $path)
+	echo (dirname $path) → created
+end
+
+if test -e $path
+	echo $path → already exists
+else
+	ln -s $this_dir/init.vim $path
+	echo $path → symlinked
+end
