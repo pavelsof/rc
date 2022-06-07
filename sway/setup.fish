31c9c132117cade3d54e29072eac171b1c74c29d
pavelsof <mail@pavelsof.com>
2022-06-07 23:38:39 +0200

    sway: outputs config.

--- sway/setup.fish
+++ sway/setup.fish
@@ -3,6 +3,7 @@
 set repo_dir (realpath (dirname (status -f)))
 set config_dir $HOME/.config/sway
 
+# the sway config dir
 if test -d $config_dir
 	echo $config_dir → already exists
 else
@@ -10,9 +11,18 @@ else
 	echo $config_dir → created
 end
 
+# the main config file
 if test -e $config_dir/config
 	echo $config_dir/config → already exists
 else
 	ln -s $repo_dir/config $config_dir/config
 	echo $config_dir/config → symlinked
+end
+
+# the outputs config
+if test -e $config_dir/outputs
+	echo $config_dir/outputs → already exists
+else
+	touch $config_dir/outputs
+	echo $config_dir/outputs → touched
 end
