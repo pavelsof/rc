243e2cf8e5dce0e967785f89ff24556b904584a9
pavelsof <mail@pavelsof.com>
2025-08-24 18:45:24 +0200

    fish: prepend ~/bin to the $PATH conditionally.

--- fish/config.fish
+++ fish/config.fish
@@ -33,7 +33,9 @@ set -gx LC_TIME $my_locale
 set -gx EDITOR vim
 
 # add ~/bin to the PATH
-set -gx PATH ~/bin $PATH
+if not contains (realpath ~/bin) $PATH
+	set -gx PATH ~/bin $PATH
+end
 
 # on macOS: add the homebrew env vars and paths
 if test (uname -s | grep Darwin)
